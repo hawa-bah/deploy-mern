fd828011046f78de159c473a58779bc48fb03631
Hawa BahBah <hawabahbah25@gmail.com>
2021-03-24 11:27:22 +0000

    changed port

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 //import routes
 // require("./routes/quoteRoutes.js")(app);
 
-const PORT = process.env.PORT || "3000";
+const PORT = process.env.PORT || "5000";
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
