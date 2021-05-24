ddf1df5ba38e664d14d41c4d7f990530bd4f1d7d
tia.1116 <tia.wang16@gmail.com>
2021-05-24 21:20:40 +1200

    heroku

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const path = require("path");
-const cors = require("cors");
-const app = express();
 require("dotenv").config();
 
+const cors = require("cors");
+
+const app = express();
 app.use(cors());
 
+//import your models
+require("./models/quote");
+
 mongoose
-  .connect("mongodb+srv://SEEDS:rN4oGfM1Iw5qFBL1@seedsteam18.eyuov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    process.env.MONGODB_CONNECTION_STRING,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("MongoDB has been connected"))
   .catch((err) => console.log(err));
 
-  // Step 1:
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//import routes
+require("./routes/quoteRoute.js")(app);
+
+const PORT = process.env.PORT || 5000;
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-//middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.json({ extended: false }));
-
-//import routes
-const articles = require('./routes/api/articles');
-app.use('/api/articles', articles);
-app.use(cors({ origin: true, credentials: true }));
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
